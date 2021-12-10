ee910cbec4716efc83f9d8de84dfe07534ba9bc3
Barjas11 <barjas205@gmail.com>
2021-12-09 23:56:32 -0500

    TinyApp

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -2,18 +2,18 @@ const { assert } = require('chai');
 
 const { getUserByEmail } = require('../helpers.js');
 
-const testUsers = {
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
-  },
-  "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
-  }
-};
+// const testUsers = {
+//   "userRandomID": {
+//     id: "userRandomID", 
+//     email: "user@example.com", 
+//     password: "purple-monkey-dinosaur"
+//   },
+//   "user2RandomID": {
+//     id: "user2RandomID", 
+//     email: "user2@example.com", 
+//     password: "dishwasher-funk"
+//   }
+// };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
